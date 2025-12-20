1b1969b79a4fb3327a238c25b79daad2aec7694c
idomyhomework <alex.scheglov25@gmail.com>
2025-12-21 00:47:46 +0100

    added input information to the contstants/index.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,107 +1,34 @@
 import React from "react";
-import { contactInput } from "../constants";
+import { contactInput } from "../constants/index.js";
 
 const Contact = () => {
+const ContactForms = () => {
+  return(
+    contactInput.map((item) => ( 
+      <div key={item.id}>
+        <label className="block mb-2.5 text-sm font-medium text-heading" htmlFor={item.name}>
+          {item.label}
+        </label>
+        <input 
+          name={item.name} 
+          type={item.type}
+          placeholder={item.placeholder}
+          pattern={item.pattern}
+          required={item.isRequired}
+          className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+        />
+      </div>
+    ))
+  )
+}
+
   return (
     <section>
-      <div className="w-9/12 mx-auto py-12">
+      <div className="w-9/12 mx-auto pt-12">
         <h2 className="font-mono mb-6">Get In Touch</h2>
         <form>
-          <div>
-            <div class="grid gap-6 mb-4 md:grid-cols-2 font-mono">
-              <div>
-                <label
-                  for="first_name"
-                  class="block mb-2.5 text-sm font-medium text-heading"
-                >
-                  First name
-                </label>
-                <input
-                  type="text"
-                  id="first_name"
-                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                  placeholder="Leo"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  for="last_name"
-                  class="block mb-2.5 text-sm font-medium text-heading"
-                >
-                  Last name
-                </label>
-                <input
-                  type="text"
-                  id="last_name"
-                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                  placeholder="Messi"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  for="company"
-                  class="block mb-2.5 text-sm font-medium text-heading"
-                >
-                  Company
-                </label>
-                <input
-                  type="text"
-                  id="company"
-                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                  placeholder=""
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  for="phone"
-                  class="block mb-2.5 text-sm font-medium text-heading"
-                >
-                  Phone number
-                </label>
-                <input
-                  type="tel"
-                  id="phone"
-                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                  placeholder="123-45-678"
-                  pattern="^\+?[0-9\s\-()]{7,20}$"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  for="website"
-                  class="block mb-2.5 text-sm font-medium text-heading"
-                >
-                  Website URL
-                </label>
-                <input
-                  type="url"
-                  id="website"
-                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                  placeholder="idomyhomework.com"
-                  required
-                />
-              </div>
-
-              <div class="mb-4">
-                <label
-                  for="email"
-                  class="block mb-2.5 text-sm font-medium text-heading"
-                >
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                  placeholder="john.doe@company.com"
-                  required
-                />
-              </div>
-            </div>
+          <div class="grid gap-6 mb-4 md:grid-cols-2 font-mono">
+            <ContactForms />
           </div>
           <div class="flex items-start mb-4">
             <div class="flex items-center h-5">
