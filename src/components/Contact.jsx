8bd7c2bc9439e7e59452b43416fdc312e1051662
idomyhomework <alex.scheglov25@gmail.com>
2025-12-21 00:22:38 +0100

    Saving before making important changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,104 +1,107 @@
 import React from "react";
+import { contactInput } from "../constants";
 
 const Contact = () => {
   return (
     <section>
       <div className="w-9/12 mx-auto py-12">
         <h2 className="font-mono mb-6">Get In Touch</h2>
-
         <form>
-          <div class="grid gap-6 mb-4 md:grid-cols-2 font-mono">
-            <div>
-              <label
-                for="first_name"
-                class="block mb-2.5 text-sm font-medium text-heading"
-              >
-                First name
-              </label>
-              <input
-                type="text"
-                id="first_name"
-                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                placeholder="Leo"
-                required
-              />
+          <div>
+            <div class="grid gap-6 mb-4 md:grid-cols-2 font-mono">
+              <div>
+                <label
+                  for="first_name"
+                  class="block mb-2.5 text-sm font-medium text-heading"
+                >
+                  First name
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                  placeholder="Leo"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="last_name"
+                  class="block mb-2.5 text-sm font-medium text-heading"
+                >
+                  Last name
+                </label>
+                <input
+                  type="text"
+                  id="last_name"
+                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                  placeholder="Messi"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="company"
+                  class="block mb-2.5 text-sm font-medium text-heading"
+                >
+                  Company
+                </label>
+                <input
+                  type="text"
+                  id="company"
+                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                  placeholder=""
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="phone"
+                  class="block mb-2.5 text-sm font-medium text-heading"
+                >
+                  Phone number
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                  placeholder="123-45-678"
+                  pattern="^\+?[0-9\s\-()]{7,20}$"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="website"
+                  class="block mb-2.5 text-sm font-medium text-heading"
+                >
+                  Website URL
+                </label>
+                <input
+                  type="url"
+                  id="website"
+                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                  placeholder="idomyhomework.com"
+                  required
+                />
+              </div>
+
+              <div class="mb-4">
+                <label
+                  for="email"
+                  class="block mb-2.5 text-sm font-medium text-heading"
+                >
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                  placeholder="john.doe@company.com"
+                  required
+                />
+              </div>
             </div>
-            <div>
-              <label
-                for="last_name"
-                class="block mb-2.5 text-sm font-medium text-heading"
-              >
-                Last name
-              </label>
-              <input
-                type="text"
-                id="last_name"
-                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                placeholder="Messi"
-                required
-              />
-            </div>
-            <div>
-              <label
-                for="company"
-                class="block mb-2.5 text-sm font-medium text-heading"
-              >
-                Company
-              </label>
-              <input
-                type="text"
-                id="company"
-                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                placeholder=""
-                required
-              />
-            </div>
-            <div>
-              <label
-                for="phone"
-                class="block mb-2.5 text-sm font-medium text-heading"
-              >
-                Phone number
-              </label>
-              <input
-                type="tel"
-                id="phone"
-                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                placeholder="123-45-678"
-                pattern="^\+?[0-9\s\-()]{7,20}$"
-                required
-              />
-            </div>
-            <div>
-              <label
-                for="website"
-                class="block mb-2.5 text-sm font-medium text-heading"
-              >
-                Website URL
-              </label>
-              <input
-                type="url"
-                id="website"
-                class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-                placeholder="idomyhomework.com"
-                required
-              />
-            </div>
-          </div>
-          <div class="mb-4">
-            <label
-              for="email"
-              class="block mb-2.5 text-sm font-medium text-heading"
-            >
-              Email address
-            </label>
-            <input
-              type="email"
-              id="email"
-              class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
-              placeholder="john.doe@company.com"
-              required
-            />
           </div>
           <div class="flex items-start mb-4">
             <div class="flex items-center h-5">
